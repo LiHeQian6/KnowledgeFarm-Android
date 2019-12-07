b3b634486933f96af9c29561ddb938245e9977ea
zhangshuaihua2017 <1764056039@qq.com>
2019-12-07 13:52:56 +0800

    subjectActivity的返回和到math的跳转 mathActivity的返回

--- KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Study/MathActivity.java
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Study/MathActivity.java
@@ -2,15 +2,58 @@ package com.li.knowledgefarm.Study;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.li.knowledgefarm.MainActivity;
 import com.li.knowledgefarm.R;
 
 public class MathActivity extends AppCompatActivity {
+    /** 返回按钮*/
+    private Button btnReturn;
+    /** 自定义点击事件监听器*/
+    private CustomerListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math);
+
+        /** 加载视图*/
+        getViews();
+        /** 注册点击事件监听器*/
+        registListener();
     }
+
+    class CustomerListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnReturn:
+                    Intent intent = new Intent();
+                    intent.setClass(MathActivity.this, SubjectListActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 加载视图
+     */
+    private void getViews(){
+        btnReturn = findViewById(R.id.btnReturn);
+    }
+
+    /**
+     * 注册点击事件监听器
+     */
+    private void registListener(){
+        listener = new CustomerListener();
+        btnReturn.setOnClickListener(listener);
+    }
+
 }
--- KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Study/SubjectListActivity.java
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Study/SubjectListActivity.java
@@ -2,15 +2,66 @@ package com.li.knowledgefarm.Study;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.li.knowledgefarm.MainActivity;
 import com.li.knowledgefarm.R;
+import com.li.knowledgefarm.Settings.SettingActivity;
 
 public class SubjectListActivity extends AppCompatActivity {
+    /** 返回按钮*/
+    private Button btnReturn;
+    private Button btnMath;
+    /** 自定义点击事件监听器*/
+    private CustomerListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+
+        /** 加载视图*/
+        getViews();
+        /** 注册点击事件监听器*/
+        registListener();
+    }
+
+    class CustomerListener implements View.OnClickListener{
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.btnReturn:
+                    Intent intent = new Intent();
+                    intent.setClass(SubjectListActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btnMath:
+                    Intent intent1 = new Intent();
+                    intent1.setClass(SubjectListActivity.this,MathActivity.class);
+                    startActivity(intent1);
+                    break;
+            }
+        }
+    }
+
+    /**
+     * 加载视图
+     */
+    private void getViews(){
+        btnReturn = findViewById(R.id.btnReturn);
+        btnMath = findViewById(R.id.btnMath);
+    }
+
+    /**
+     * 注册点击事件监听器
+     */
+    private void registListener(){
+        listener = new CustomerListener();
+        btnReturn.setOnClickListener(listener);
+        btnMath.setOnClickListener(listener);
     }
 }
