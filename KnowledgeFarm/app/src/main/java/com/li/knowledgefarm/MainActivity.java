ba134952bcdc88d2031a601ba9659dd7b55a29d8
859684581@qq.com <ziyousjw5211314>
2019-12-07 11:40:31 +0800

    完成商店页面网络访问请求

--- KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/MainActivity.java
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/MainActivity.java
@@ -2,7 +2,10 @@ package com.li.knowledgefarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.li.knowledgefarm.Shop.ShopActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, ShopActivity.class);
+        startActivity(intent);
     }
 }
--- KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Shop/ShopActivity.java
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Shop/ShopActivity.java
@@ -2,23 +2,69 @@ package com.li.knowledgefarm.Shop;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.li.knowledgefarm.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ShopActivity extends AppCompatActivity {
+
+    private OkHttpClient okHttpClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        okHttpClient = new OkHttpClient();
         setStatusBar();
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                getShopingsItems();
+            }
+        }.start();
     }
 
 
+    /**
+     * @Description 获取商品数据
+     * @Auther 孙建旺
+     * @Date 上午 10:51 2019/12/07
+     * @Param []
+     * @return void
+     */
+    private void getShopingsItems() {
+        Request request = new Request.Builder()
+                .url("http://"+getResources().getString(R.string.IP)+":8080/FarmKnowledge/crop/initCrop")
+                .build();
+        Log.i("Ip","http://"+getResources().getString(R.string.IP)+":8080/FarmKnowledge/crop/initCrop");
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(@NotNull okhttp3.Call call, @NotNull Response response) throws IOException {
+                String shopItemMessages = response.body().string();
+                Log.i("shop",shopItemMessages);
+            }
+            @Override
+            public void onFailure(@NotNull okhttp3.Call call, @NotNull IOException e) {
+                Log.i("error","请求失败！");
+            }
+        });
+    }
     /**
      * @Description 设置状态栏状态
      * @Auther 孙建旺
