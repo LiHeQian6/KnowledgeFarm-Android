bd42675ffcf0deee5776709628786a00a1c4ffbb
zhangshuaihua2017 <1764056039@qq.com>
2019-12-09 19:32:26 +0800

    修改后台界面

---
+++ FarmKnowledge/src/com/farm/entity/BagCropItem.java
@@ -0,0 +1,30 @@
+package com.farm.entity;
+
+import com.farm.model.Crop;
+
+public class BagCropItem {
+	private int number;
+	private Crop crop;
+
+	public BagCropItem() {
+		super();
+	}
+	public BagCropItem(int number, Crop crop) {
+		super();
+		this.number = number;
+		this.crop = crop;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public Crop getCrop() {
+		return crop;
+	}
+	public void setCrop(Crop crop) {
+		this.crop = crop;
+	}
+	
+}
