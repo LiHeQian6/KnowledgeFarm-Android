0a3f48362500daa0876ec31e1a34c25079c4f1c2
859684581@qq.com <ziyousjw5211314>
2019-12-10 14:30:44 +0800

    设置注册页面通知栏状态

--- KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Login/QQFirstActivity.java
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Login/QQFirstActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.li.knowledgefarm.R;
@@ -19,6 +20,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -84,7 +86,7 @@ public class QQFirstActivity extends AppCompatActivity {
         if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
-
+        setStatusBar();
         Intent intent = getIntent();
         openId = intent.getStringExtra("opId");
         Nickname = intent.getStringExtra("nick");
@@ -165,6 +167,21 @@ public class QQFirstActivity extends AppCompatActivity {
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
             grade = "一年级";
+        }
+    }
+
+    /**
+     * @Description 设置通知栏状态
+     * @Auther 孙建旺
+     * @Date 下午 2:29 2019/12/10
+     * @Param []
+     * @return void
+     */
+    protected void setStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//隐藏状态栏但不隐藏状态栏字体
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //隐藏状态栏，并且不显示字体
+            //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//实现状态栏文字颜色为暗色
         }
     }
 }
