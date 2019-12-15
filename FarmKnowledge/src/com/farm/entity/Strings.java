ff9c37eccacc25f9f519e6c23462bdda3293f4d8
zhangshuaihua2017 <1764056039@qq.com>
2019-12-15 19:52:23 +0800

    修改路径

--- FarmKnowledge/src/com/farm/entity/Strings.java
+++ FarmKnowledge/src/com/farm/entity/Strings.java
@@ -6,12 +6,12 @@ public class Strings {
 	 * linux服务器
 	 */
 	//作物图片
-	public static String cropPhotoUrl1 = "http://39.106.18.238:8080/img/";
-	public static String cropfilePath1 = "/home/admin/Tomcat/apache-tomcat-8.5.39/webapps/img/";
+	public static String cropPhotoUrl = "http://39.106.18.238:8080/img/";
+	public static String cropfilePath = "/home/admin/Tomcat/apache-tomcat-8.5.39/webapps/img/";
 	
 	//用户头像
-	public static String userPhotoUrl1 = "http://39.106.18.238:8080/photo/";
-	public static String userfilePath1 = "/home/admin/Tomcat/apache-tomcat-8.5.39/webapps/photo/";
+	public static String userPhotoUrl = "http://39.106.18.238:8080/photo/";
+	public static String userfilePath = "/home/admin/Tomcat/apache-tomcat-8.5.39/webapps/photo/";
 	
 	//用户等级
 	public static int userLevel[] = {0,100,300,500,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000};
@@ -20,11 +20,11 @@ public class Strings {
 	 * 本机服务器
 	 */
 	//作物图片
-	public static String cropPhotoUrl = "http://10.7.87.220:8080/img/";
-	public static String cropfilePath = "D:/eclipse/Workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ROOT/img/";
+	public static String cropPhotoUrl1 = "http://10.7.87.220:8080/img/";
+	public static String cropfilePath1 = "D:/eclipse/Workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ROOT/img/";
 	
 	//用户头像
-	public static String userPhotoUrl = "http://10.7.87.220:8080/photo/";
-	public static String userfilePath = "D:/eclipse/Workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ROOT/photo/";
+	public static String userPhotoUrl1 = "http://10.7.87.220:8080/photo/";
+	public static String userfilePath1 = "D:/eclipse/Workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/ROOT/photo/";
 	
 }
