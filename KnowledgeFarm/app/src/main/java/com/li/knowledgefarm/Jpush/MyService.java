4e5593bebc8d90df4835fc1c6a5e9ef82a135910
Sunjianwang <859684581@qq.com>
2020-04-21 15:22:58 +0800

    集成Jpush

---
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Jpush/MyService.java
@@ -0,0 +1,12 @@
+package com.li.knowledgefarm.Jpush;
+
+import cn.jpush.android.service.JCommonService;
+
+/**
+ * @author 孙建旺
+ * @description
+ * @date 2020/04/21 下午3:07
+ */
+
+public class MyService extends JCommonService {
+}
---
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Jpush/Myapplication.java
@@ -0,0 +1,21 @@
+package com.li.knowledgefarm.Jpush;
+
+import android.app.Application;
+
+import cn.jpush.android.api.JPushInterface;
+
+/**
+ * @author 孙建旺
+ * @description
+ * @date 2020/04/21 下午3:00
+ */
+
+public class Myapplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+    }
+}
