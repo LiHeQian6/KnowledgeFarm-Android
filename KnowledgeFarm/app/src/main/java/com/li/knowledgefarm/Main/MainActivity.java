23f308224b760726905672676a82a28a776f04a9
LiHeQian6 <1142808063@qq.com>
2020-05-03 15:35:43 +0800

    适配土地和弹窗大小
    Signed-off-by: LiHeQian6 <1142808063@qq.com>

--- KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Main/MainActivity.java
+++ KnowledgeFarm/app/src/main/java/com/li/knowledgefarm/Main/MainActivity.java
@@ -20,6 +20,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -120,8 +121,8 @@ public class MainActivity extends AppCompatActivity {
     private NotifyActivity notifyActivity;
     private UserMessagePopUp userMessagePopUp;
     private ListView notify_list_view;
-    private float LAND_WIDTH_2 = 224;
-    private float LAND_HEIGHT_2 = 114;
+    private float LAND_WIDTH_2;
+    private float LAND_HEIGHT_2;
     private Handler friendMessagesHandler;
     private ImageView dayTask;
     private DayTaskPopUpWindow dayTaskPopUpWindow;
@@ -539,7 +540,14 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 18; i++) {
             LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
             final View landGroup = inflater.inflate(R.layout.land_group, null);
-            landGroup.setLayoutParams(new FrameLayout.LayoutParams(480, 240));
+            int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                    120, getResources().getDisplayMetrics());
+            int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                    60, getResources().getDisplayMetrics());
+            System.out.println(width+"   "+height);
+            LAND_WIDTH_2=width/2.0f-12;
+            LAND_HEIGHT_2=height/2.0f-6;
+            landGroup.setLayoutParams(new FrameLayout.LayoutParams(width, height));
             int num = i + 1;
             if ((num - 1) % 3 == 0) {
                 x = ((num - 1) / 3) * LAND_WIDTH_2 + LAND_WIDTH_2 * 2;
